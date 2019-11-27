28215a7b67c37bd93c6bfe768718b70161ee7006
VLAD <golo.vv@yandex.by>
2019-11-28 00:02:22 +0300

    Task 1 made

---
+++ lesson 1/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let money;
+let time;
+
+while(isNaN(money) || typeof money != "number"){
+    money = parseInt(prompt("Ваш бюджет на месяц?", 0), 10);
+    console.log(money);
+    console.log(typeof money);
+    if(isNaN(money)){
+        alert("Бюджет должен быть числом, введите еще раз!");
+    }
+    console.log(isNaN(money));
+    console.log(typeof money != "number");
+} 
+
+time = new Date (prompt("Введите дату в формате YYYY-MM-DD?", "2019-11-27"));
+
+let expenses = {};
+let keyExpenses = prompt("Введите обязательную статью расходов в этом месяце", 'Кот');
+expenses[keyExpenses] = prompt("Во сколько обойдется?", 0);
+
+let optionalExpenses = {};
+
+let income = [];
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: expenses,
+    optionalExpenses: optionalExpenses,
+    income: income,
+    savings: false
+}
+
+alert((appData.budget / 30).toFixed(2));
