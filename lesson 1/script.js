0e3ce8281c726a4deabd5237fd807c19b352a9c6
VLAD <golo.vv@yandex.by>
2019-12-01 21:51:20 +0300

    lesson 2 folder add

--- lesson 1/script.js
+++ lesson 1/script.js
@@ -8,7 +8,7 @@ alert( +"Infinity" );
 console.log(0 || "" || 2 || undefined || true || falsе);
 
 while(isNaN(money) || typeof money != "number"){
-    money = parseInt(prompt("Ваш бюджет на месяц?", 0), 10);
+    money = +parseInt(prompt("Ваш бюджет на месяц?", 0), 10);
     console.log(money);
     console.log(typeof money);
     if(isNaN(money)){
@@ -37,4 +37,49 @@ let appData = {
     savings: false
 }
 
-alert((appData.budget / 30).toFixed(2));
+appData.moneyPerDay = (appData.budget / 30).toFixed(2);
+alert(appData.moneyPerDay);
+
+for(let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = +prompt("Во сколько обойдется?", ''); 
+
+        if( (typeof(a))=== "string" && (typeof(a)) != null && (typeof(b)) !=null
+            && a != "" && b != "" && a.length < 50){
+                console.log("done");
+                appData.expenses[a] = b;
+        } else{
+           i--;
+           console.log("No done");
+        } 
+}
+
+let i = 0;
+while (i < 2){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    b = +prompt("Во сколько обойдется?", ''); 
+
+    if( (typeof(a))=== "string" && (typeof(a)) != null && (typeof(b)) !=null
+        && a != "" && b != "" && a.length < 50){
+            i++;
+            console.log("done");
+            appData.expenses[a] = b;
+    } else{
+       console.log("No done");
+    } 
+}
+
+let i = 0;
+while (i < 2){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    b = +prompt("Во сколько обойдется?", ''); 
+
+    if( (typeof(a))=== "string" && (typeof(a)) != null && (typeof(b)) !=null
+        && a != "" && b != "" && a.length < 50){
+            i++;
+            console.log("done");
+            appData.expenses[a] = b;
+    } else{
+       console.log("No done");
+    } 
+}
